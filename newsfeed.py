9b4a0d5433ec6fc6dc7d6446530dc7d92217d0e7
Rolf <>
2018-05-04 00:01:56 +0200

    Add comments to the configuration

--- newsfeed.py
+++ newsfeed.py
@@ -41,20 +41,22 @@ from urlparse import urlparse
 mydirectory = os.path.dirname(os.path.realpath(__file__))
 myconfig = mydirectory + "/newsfeed.config"
 
+
 def readoldpostids(idfile):
     """
         Loads already processed ids from file.
     """
-    
+
     # load ids already parsed
     try:
         text_file = open(idfile, "r")
-        postids = json.loads(text_file.read())		
+        postids = json.loads(text_file.read())
         text_file.close()
     except IOError:
         print "no ids file, start with empty list."
         postids = []
     return postids
+
 
 def writeoldpostids(idfile, postids):
     """
@@ -64,21 +66,23 @@ def writeoldpostids(idfile, postids):
     text_file.write(json.dumps(postids))
     text_file.close()
 
+
 def containsanyword(filter, entry):
     if filter is None:
         return True
-    
+
     for word in filter:
         if word.lower() in entry.title.lower() or word.lower() in entry.summary.lower():
             return True
-    
+
     return False
+
 
 def getrssentry(feedurl, oldpostids, filter):
     """
         Loads rss entries from feedurl, and returns
         the one feed that is not in postids
-    """        
+    """
 
     socket.setdefaulttimeout(120)
 
@@ -88,7 +92,7 @@ def getrssentry(feedurl, oldpostids, filter):
 
     # Find the oldest post that is new to us.
     for entry in rssentries:
-        
+
         if 'id' not in entry:
             # No id, generating hash based on link url.
             entry.id = abs(hash(entry.link)) % (10 ** 8)
@@ -101,48 +105,68 @@ def getrssentry(feedurl, oldpostids, filter):
     # No new entry found, return none.
     return None
 
+
 def postrssentry(username, entry):
     """
         Posts the entry to mattermost. Returns True if posted
         succesfuly, or False if not.
     """
     payload = {
-        'username' : username,
-        'text' : "[" + entry.title.encode('utf-8') +"](" 
-                    + entry.link.encode('utf-8') + ") "
-                    + entry.summary.encode('utf-8')
+        'username': username,
+        'text': "[" + entry.title.encode('utf-8') + "]("
+        + entry.link.encode('utf-8') + ") "
+        + entry.summary.encode('utf-8')
     }
 
     # print payload
     # return False
-    resp = requests.post(mattermosturl, data=json.dumps(payload))
-    return (resp.status_code == 200)
+    try:
+        resp = requests.post(mattermosturl, data=json.dumps(payload))
+        return (resp.status_code == 200)
+    except:
+        print "Failed to post to " + mattermosturl
+        return False
+
 
 def removehtml(summary):
     stripped = re.sub('<p>Het bericht <a href.+</p>', '', summary)
     stripped = re.sub('<[^<]+?>', '', stripped)
     return stripped
 
+
 def writeconfig(config):
     """Writes the config to json file"""
-    json.dump(config, open(myconfig,'w'))
+    json.dump(config, open(myconfig, 'w'))
+
 
 def readconfig():
     """Reads config from json file"""
-    return json.load(open(myconfig))
+
+    cleanconfig = ""
+    with open(myconfig) as f:
+        content = f.readlines()
+        content = [re.sub('#.*', '', x) for x in content] 
+
+        cleanconfig = "".join(content)
+    
+    return json.loads(cleanconfig)
+
 
 if __name__ == "__main__":
     # Raw, no checking. Ugly errors when you don't use this script
     # in the form: newsfeed.py <rss link> <mattermost webhook url>
 
     config = readconfig()
-    
-    mattermosturl = config['webhook']
+
+    if 'webhook' in config:
+        mattermosturl = config['webhook']
+    else:
+        mattermosturl = None
 
     for feed in config['feeds']:
         feedurl = feed['feedurl']
         filter = None
-        if  'filter' in feed:
+        if 'filter' in feed:
             filter = feed['filter']
 
         idfile = mydirectory + "/" + urlparse(feedurl).hostname + ".ids"
@@ -153,9 +177,13 @@ if __name__ == "__main__":
         username = urlparse(feedurl).hostname
         username = re.sub('(^www\\.)|(^feeds\\.)|(^zoek\\.)', '', username)
 
+        if mattermosturl is None:
+            # Print the entry and skip actual posting.
+            print entry
+            continue
+
         if entry is not None and postrssentry(username, entry):
             postids = postids + [entry.id]
             writeoldpostids(idfile, postids)
         else:
             print "No new entry."
-
